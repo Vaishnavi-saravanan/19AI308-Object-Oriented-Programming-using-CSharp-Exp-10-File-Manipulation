02a621395eff0da76d0d26146de9127b2f4d24bc
VaishnaviSaravanan <118541897+Vaishnavi-saravanan@users.noreply.github.com>
2024-05-15 11:40:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
-using System;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-namespace DemoApplication
-{
-    struct student
-    {
-           // Write Your Code Here
-    };
-    class Tutorial
-    {
 
-        static void Main(string[] args)
+using System;
+
+struct student
+{
+    public string name;
+
+    public int age;
+
+    public int marks;
+};
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        student[] s  = new student[5];
+        for (int i=0; i < 5; i++)
         {
-            // Write Your Code Here
+            s[i].name = Console.ReadLine();
+            s[i].age = Convert.ToInt32(Console.ReadLine());
+            s[i].marks = Convert.ToInt32(Console.ReadLine());
         }
+        string path = @"C:\studentInfo.txt";
+        using (StreamWriter sw = File.AppendText(path))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                sw.WriteLine(s[i].name + " " + s[i].age + " " + s[i].marks);
+            }
+        }
+        Console.WriteLine(File.ReadAllText(path));
     }
 }
